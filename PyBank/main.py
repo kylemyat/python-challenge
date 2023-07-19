8ae91a7e381bfbc52e0f45e371bf27a1b7a9079e
kylemyat <kyle.myatg@gmail.com>
2023-07-18 21:16:51 -0700

    final versions

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,36 +3,40 @@ import csv
 from statistics import mean
 
 budget_csv = os.path.join(r'C:\Users\kylem\Desktop\Bootcamp_Assignments\python-challenge\PyBank\Resources\budget_data.csv')
-
+output_text = os.path.join(r'C:\Users\kylem\Desktop\Bootcamp_Assignments\python-challenge\PyBank\analysis\pybank.txt')
 
 #Opens and reads budget_data.csv
 with open(budget_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter= ',')
     csv_header = next(csvreader)
     
-    #Total Months
+    #sets "months" and "net" to 0 to count in the loop.
+    #Created lists for profit, months and change in profit as I will be using these lists in determining the greatest increase, decrease and the months associated with those changes
+    
     month = 0
     net = 0
     profit = []
     change = []
-    previous_profit = 0
     month_list = []
 
     for row in csvreader:
 
-        #adding all the rows
+        #adding all the rows except the header row to show total months
         month += 1
 
-        # sum of the profit/loss column to provide net
+        # sum of the profit/loss column to provide net profit
         net += float(row[1])
 
-        #creates a list of profits/losses 
+        #creates a list of profits/losses. This list is later used to find the difference between profits per month
         profit.append(float(row[1]))
+        
+        #creates a list of months which is later used when determining which month had the largest increase and decrease in profits
         month_list.append(str(row[0]))
         
 
-        #Loops throught the "profit" list, finds the difference between the values and returns the average
-        
+        #Loops throught the "profit" list, finds the difference between the values and adds each difference to a list named "change"
+        # The index of the greatest change value in the "change" list is compared with the index of the "month_list" to retrieve the month associated with greatest increase and decrease of profits
+
     for i in range(1,len(profit)):
         change.append(profit[i] - profit [i-1])
         ave_change = round(sum(change) / len(change), 2)
@@ -43,13 +47,18 @@ with open(budget_csv) as csvfile:
         location_2 = change.index(greatest_decrease)
         month_decrease = month_list[int(location_2) +1]
 
-    
-    
-print ("Total Months: ",month)
-print ("Total: $",net)
-print ("Average Change: $",ave_change)
-print ("Greatest Increase in Profits:", month_increase,"$", greatest_increase)
-print ("Greatest Decrease in Profits:", month_decrease, "$", greatest_decrease)
+
+f = open(output_text, 'a')
+print("Financial Analysis", file=f)
+print("----------------------", file=f)   
+print ("Total Months: ",month, file=f)
+print ("Total: $",net, file=f)
+print ("Average Change: $",ave_change, file=f)
+print ("Greatest Increase in Profits:", month_increase,"$", "(",greatest_increase,")", file=f)
+print ("Greatest Decrease in Profits:", month_decrease, "$", "(",greatest_decrease,")", file=f)
+f.close()
+
+
 
 
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,41 +3,47 @@ import csv
 
 
 election_csv = os.path.join(r'C:\Users\kylem\Desktop\Bootcamp_Assignments\python-challenge\PyPoll\Resources\election_data.csv')
-
+output_text = os.path.join(r'C:\Users\kylem\Desktop\Bootcamp_Assignments\python-challenge\PyPoll\analysis\pypoll.txt')
 
 #opens and reads election_data.csv
 with open(election_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter= ',')
     csv_header = next(csvreader)
 
+    #List of unique candidate names to determine the number of candidates running
     candidates = []
+
+    #List of candidate names as seen in the CSV fiie. This list is used to determine the votes each candidate received based on the number of times the name appears in the list.
     all_candidates = []
     vote_1 = 0
     vote_2 = 0
     vote_3 = 0
     
     
-    #Total number of votes
+    #Total number of votes set to 0 to add later
     votes = 0
     
     for row in csvreader:
 
-        #adding all the rows
+        #adding all the rows to tally the total number of votes
         votes += 1
+
+        #creating a new list of only candidate names. Each time the name appears in the list will be used to determine the number of votes for each candidate
         all_candidates.append(str(row[2]))
         
         
-
+        #If statement to loop through the rows of the CSV to determine unique candidate names which determines the number of candidates
         if row[2] not in candidates:
             candidates.append(row[2])
-           
+
+    #Each candidate is held in their own variable based on the index in "candidates" list
     candidate_1 = candidates[0]
     candidate_2 = candidates[1]
     candidate_3 = candidates[2]
     running = len(candidates)
 
     
-    
+    #For loop of the "all_candidates" list to count the numbers of votes each candidate received
     for x in all_candidates:
         if x == candidate_1:
             vote_1 = vote_1 + 1
@@ -45,7 +51,7 @@ with open(election_csv) as csvfile:
             vote_2 = vote_2 + 1
         if x == candidate_3:
             vote_3 = vote_3 + 1
-
+#To determine the Winner of the election, the IF statement will compare the number of votes each candidate received
 win = max(vote_1, vote_2, vote_3)
 if win == vote_1:
     winner = candidate_1
@@ -54,17 +60,19 @@ if win == vote_2:
 if win == vote_3:
     winner = candidate_3
 
+#Used a new variable to hold the percentage of votes each candidate received
 vote_1p = round((vote_1 / votes * 100),3)
 vote_2p = round((vote_2 / votes * 100),3)
 vote_3p = round((vote_3 / votes * 100),3)
 
-    
-print("Election Results")
-print("--------------------")
-print("Total Votes: ", votes)
-print("--------------------")
-print(candidate_1,":", vote_1p, "%", " (",vote_1,") ")
-print(candidate_2,":", vote_2p, "%", " (",vote_2,") ")
-print(candidate_3,":", vote_3p, "%", " (",vote_3,") ")
-print("--------------------")
-print("Winner: ", winner)
+f = open(output_text, 'a')    
+print("Election Results", file=f)
+print("--------------------", file=f)
+print("Total Votes: ", votes, file=f)
+print("--------------------", file=f)
+print(candidate_1,":", vote_1p, "%", " (",vote_1,") ", file=f)
+print(candidate_2,":", vote_2p, "%", " (",vote_2,") ", file=f)
+print(candidate_3,":", vote_3p, "%", " (",vote_3,") ", file=f)
+print("--------------------", file=f)
+print("Winner: ", winner, file=f)
+f.close()
